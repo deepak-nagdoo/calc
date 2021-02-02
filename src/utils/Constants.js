6973e0e9ada108dae0325e63b0188afc5cbb7a2c
deepakvishwakarma <deepakvishwakarma@goniyo.com>
2021-02-02 15:21:34 +0530

    Final

---
+++ src/utils/Constants.js
@@ -0,0 +1,8 @@
+export default {
+  OPS: {
+    MULTIPLY: "MULTIPLY",
+    ADDITION: "ADDITION",
+    SUBTRACT: "SUBTRACT",
+    DIVIDE: "DIVIDE",
+  },
+};
---
+++ src/utils/Service.js
@@ -0,0 +1,5 @@
+export const square = (num) => Math.pow(num, 2);
+
+export const squareRoot = (num) => Math.sqrt(num);
+
+export const sign = (num) => -num;
